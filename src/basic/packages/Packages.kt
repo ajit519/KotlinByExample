f0b576b0a00d1ea77da65132ceb5ad62ffe94198
ajit519 <ajitcse519@gmail.com>
2019-11-19 19:28:30 +0530

    packages in kotlin.

---
+++ src/basic/packages/Packages.kt
@@ -0,0 +1,22 @@
+package basic.packages
+
+//Kotlin package is similar to java. expect it didn't enforce the file should be placed in the same directory structure.
+/**
+ * Below default packages are imported in kotlin
+ * a. kotlin.*
+ * b. kotlin.annotations.*
+ * c. kotlin.collection.*
+ * d. kotlin.compression.*
+ * e. kotlin.io.*
+ * f. kotlin.ranges.*
+ * g. kotlin.sequence.*
+ * h. kotlin.text.*
+ *
+ * Based on the platform below package are imported.
+ * -jvm
+ * java.lang.*
+ * kotlin.jvm.*
+ *
+ * Similar to java we use import keyword to import the class. Additionally we have feature of alias similar to php
+ * import java.langa.Thread as javaThreadClass
+ */
